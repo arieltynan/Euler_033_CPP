bfae23ca9e98747181ed8cbde047fad7d90e9084
Ariel Tynan <tynan.a@northeastern.edu>
2022-01-25 17:36:17 -0700

    Found four solutions

--- Euler_033_CPP.cpp
+++ Euler_033_CPP.cpp
@@ -7,5 +7,23 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 11; i < 100; i++) // denominator
+    {
+        for (int j = 11; j < i; j++) // numerator
+        {
+            if (!(i % 10 == 0 || j % 10 == 0))
+            {
+                if (i / 10 == j % 10) // same digit in ones/tens place for num/den respectively
+                {
+                    if ((j / 10)*1.0 / (i % 10)*1.0 == j*1.0 / i*1.0)
+                    {
+                        cout << "Success: " << j << " / " << i << endl;
+                    }
+                }
+            }
+        }
+    }
+
+    // cout << 49 % 10 << endl; // 9
+    // cout << 49 / 10 << endl; // 4
 }
